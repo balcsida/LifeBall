25505c440106e21476f64ab68d414662fbebe982
NemethNorbert <norbert.nemeth@liferay.com>
2018-05-09 22:49:59 +0200

    dolgok

--- src/modules/NavBar.js
+++ src/modules/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import logo from '../resources/pictures/300x90.png';
+import logo from '../resources/pictures/lifeball_logo.svg';
 import UserInfo from './UserInfo.js'
 
 class NavBar extends React.Component {
@@ -8,21 +8,18 @@ class NavBar extends React.Component {
   render() {
     return (
       <div className="">
-        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+        <nav className="navbar navbar-toggleable-md bg-grad">
           <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#"><img src={logo} width="200" height="50"></img></a>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <li className="nav-item">
+                <a className="nav-link active" href="#">Games <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">Disabled</a>
+                <a className="nav-link" href="#">My Bets</a>
               </li>
             </ul>
             <UserInfo user={this.props.user}/>
